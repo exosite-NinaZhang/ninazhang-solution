bd973b977f93f4db8d0866e197efc5d7a4d62d67
NinaZhang <exosite-NinaZhang@users.noreply.github.com>
2016-10-17 18:43:35 +0800

    Modify lua

--- sample_api.lua
+++ sample_api.lua
@@ -1,9 +1,3 @@
---#ENDPOINT GET /hello/{name}
-local res = "Hi"
-local q = request.parameters.name
-response.message = res..", "..q
-
-
 --#ENDPOINT POST /timeseries/query
 local res
 local q = request.body.sql
@@ -31,7 +25,15 @@ return Keystore.clear()
 
 --#ENDPOINT POST /keystore/set
 local res = Keystore.set({key=request.body.key, value=request.body.value})
-return res
+response.message = res
+response.code = res.status
+
+--#ENDPOINT POST /keystore/set/{firstNumber}/{endNumber}
+local firstNumber = request.parameters.firstNumber
+local endNumber = request.parameters.endNumber
+for i = firstNumber, endNumber, 1 do
+Keystore.set({ key = "key" .. i, value = "value" .. i})
+end
 
 --#ENDPOINT GET /keystore/key/{key}
 res = Keystore.get({key=request.parameters.key})
@@ -135,7 +137,7 @@ response.message = User.deleteRole(parameters)
 --#ENDPOINT GET /user/listPerms
 return User.listPerms()
 
---#ENDPOINT POST /user/createPrems
+--#ENDPOINT POST /user/createPerms
 local parameters = {
   end_point = request.body.endpoint,
   method = request.body.method
@@ -157,7 +159,7 @@ return User.getUserToken(parameters)
 
 --#ENDPOINT POST /user/deletePerms
 local parameters = {
-  ["perm_id"] = request.body.permid
+  perm_id = request.body.permId
 }
 return User.deletePerm(parameters)
 
@@ -166,10 +168,10 @@ response.message = User.listUserRoles({id = request.body.id})
 
 --#ENDPOINT POST /user/assignUser
 local parameters = {
-  id = request.body.userid,
+  id = request.body.userId,
   roles = {
     {
-      role_id = request.body.roleid,
+      role_id = request.body.roleId,
       parameters = {
         {
           name = request.body.name,
@@ -183,7 +185,7 @@ response.message = User.assignUser(parameters)
 
 --#ENDPOINT POST /user/getSocial
 local parameters = {
-  consumer = requset.body.consumer
+  consumer = request.body.consumer
 }
 response.message = User.getSocial(parameters)
 
@@ -196,10 +198,166 @@ local parameters = {
 }
 return User.updateSocial(parameters)
 
+--#ENDPOINT GET /user/random
+local s = ""
+local str = ""
+local r = ""
+local randnum = 0
+local randwordlen = 0
+math.randomseed(os.time())
+randnum = math.random(1,10)
+for i = 1,randnum do
+  randwordlen = math.random(1,10)
+  str = ""
+  for j = 1,randwordlen do
+    if j == 1 then
+      local ty = math.random(1,2)
+        if ty == 1 then
+          r = math.random(65,90)
+        else
+          r = math.random(97,122)
+        end
+        r = string.char(r)
+    else
+      local ty = math.random(1,3)
+      if ty == 1 then
+        r = math.random(48,57)
+      elseif ty == 2 then
+        r = math.random(65,90)
+      else
+        r = math.random(97,122)
+      end
+      r = string.char(r)
+    end
+    str = str .. r
+  end
+  s = s .. str .. ","
+end
+return s
+
+--#ENDPOINT POST /user/createUserData
+key = request.body.key
+value = request.body.value
+parameters = {id = request.body.id}
+keyt = {}
+valuet = {}
+i=0
+if key ~= nil and value ~= nil then
+  i=0
+  for str in string.gmatch(key, "([^" .. "," .. "]+)") do
+    i=i+1
+    keyt[i] = str
+  end
+  i=0
+  for str in string.gmatch(value, "([^" .. "," .. "]+)") do
+    i=i+1
+    valuet[i] = str
+  end
+  if keyt ~= nil and valuet ~= nil then
+    for num=1,i do
+      local key_temp = keyt[num]
+      local value_temp = valuet[num]
+      parameters[key_temp] = value_temp
+    end
+    response = User.createUserData(parameters)
+  else
+    return "No value"
+  end
+end
+
+--#ENDPOINT POST /user/listUserData
+local parameters = {
+  id = request.body.id;
+}
+response.message = User.listUserData(parameters)
+
+--#ENDPOINT POST /user/updateUserData
+key = request.body.key
+value = request.body.value
+parameters = {id = request.body.id}
+keyt = {}
+valuet = {}
+i=0
+if key ~= nil and value ~= nil then
+  i=0
+  for str in string.gmatch(key, "([^" .. "," .. "]+)") do
+    i=i+1
+    keyt[i] = str
+  end
+  i=0
+  for str in string.gmatch(value, "([^" .. "," .. "]+)") do
+    i=i+1
+    valuet[i] = str
+  end
+  if keyt ~= nil and valuet ~= nil then
+    for num=1,i do
+      local key_temp = keyt[num]
+      local value_temp = valuet[num]
+      parameters[key_temp] = value_temp
+    end
+    return User.updateUserData(parameters)
+  else
+    return "No value"
+  end
+end
+
+--#ENDPOINT POST /user/getUserData
+local parameters = {
+  id = request.body.id,
+  key = request.body.key
+}
+return User.getUserData(parameters)
+
+--#ENDPOINT POST /user/deleteUserData
+local parameters = {
+  id = request.body.id,
+  keys = {request.body.keys}
+  }
+response.message = User.deleteUserData(parameters)
+
+--#ENDPOINT POST /user/listRoleUsers
+local parameters = {
+  role_id = request.body.roleId
+}
+response.message = User.listRoleUsers(parameters)
+
+--#ENDPOINT POST /user/addRoleParam
+local parameters = {
+  role_id = request.body.id,
+  body = {
+    {
+      name = request.body.name
+    }
+  }
+}
+response.message = User.addRoleParam(parameters)
+
+--#ENDPOINT POST /user/resetUserPassword
+local parameters = {
+  id = request.body.id,
+  password = request.body.password
+}
+response.message = User.resetUserPassword(parameters)
+
+--#ENDPOINT POST /user/deassignUser
+response.message = User.deassignUser(parameters)
+local parameters = {
+  id = request.body.id,
+  role_id = request.body.roleId
+}
+response.message = User.deassignUser(parameters)
+
 --#ENDPOINT POST /timer/schedule
 resp = Timer.schedule(request.body)
 response.message = resp
 response.code = resp.status
+
+response.message = User.deassignUser(parameters)
+local parameters = {
+  id = request.body.id,
+  role_id = request.body.roleId
+}
+response.message = User.deassignUser(parameters)
 
 --#ENDPOINT POST /timer/sendAfter
 response.message = Timer.sendAfter(request.body)
@@ -214,96 +372,70 @@ response.message = Timer.cancel(request.body)
 response.message = Timer.cancelAll()
 
 --#ENDPOINT POST /twilio/listCall
-local out = Twilio.listCall({
-  AccountSid = request.body.AccountSid,
-  AuthToken = request.body.AuthToken
-})
-response.message = out
+return Twilio.listCall()
 
 --#ENDPOINT POST /twilio/createCall
 local out = Twilio.createCall({
   From = request.body.From,
   To = request.body.To,
-  Url = request.body.Url,
-  AccountSid = request.body.AccountSid,
-  AuthToken = request.body.AuthToken
+  Url = request.body.Url
  })
-response.message = out
+return out
 
 --#ENDPOINT POST /twilio/listMessage
-local out = Twilio.listMessage({
-  AccountSid = request.body.AccountSid,
-  AuthToken = request.body.AuthToken
-})
-response.message = out
+return Twilio.listMessage()
 
 --#ENDPOINT POST /twilio/postMessage
 local out = Twilio.postMessage({
   From = request.body.From,
   To = request.body.To,
-  Body = request.body.Body,
-  AccountSid = request.body.AccountSid,
-  AuthToken = request.body.AuthToken
+  Body = request.body.Body
 })
-response.message = out
+return out
 
 --#ENDPOINT POST /twilio/getCall
 local parameters ={
-  CallSid = request.body.CallSid,
-  AccountSid = request.body.AccountSid,
-  AuthToken = request.body.AuthToken
+  CallSid = request.body.CallSid
 }
-response.message = Twilio.getCall(parameters)
+return Twilio.getCall(parameters)
 
 --#ENDPOINT POST /twilio/deleteCall
 local parameters ={
-  CallSid = request.body.CallSid,
-  AccountSid = request.body.AccountSid,
-  AuthToken = request.body.AuthToken
+  CallSid = request.body.CallSid
 }
-response.message = Twilio.deleteCall(parameters)
+return Twilio.deleteCall(parameters)
 
 --#ENDPOINT POST /twilio/getMessage
 local parameters ={
-  MessageSid = request.body.MessageSid,
-  AccountSid = request.body.AccountSid,
-  AuthToken = request.body.AuthToken
+  MessageSid = request.body.MessageSid
 }
-response.message = Twilio.getMessage(parameters)
+return Twilio.getMessage(parameters)
 
 --#ENDPOINT POST /twilio/deleteMessage
 local parameters ={
-  MessageSid = request.body.MessageSid,
-  AccountSid = request.body.AccountSid,
-  AuthToken = request.body.AuthToken
+  MessageSid = request.body.MessageSid
 }
-response.message = Twilio.deleteMessage(parameters)
+return Twilio.deleteMessage(parameters)
 
 --#ENDPOINT POST /twilio/listMedia
 local parameters ={
-  MessageSid = request.body.MessageSid,
-  AccountSid = request.body.AccountSid,
-  AuthToken = request.body.AuthToken
+  MessageSid = request.body.MessageSid
 }
-response.message = Twilio.listMedia(parameters)
+return Twilio.listMedia(parameters)
 
 --#ENDPOINT POST /twilio/getMedia
 local parameters ={
   MessageSid = request.body.MessageSid,
-  MediaSid = request.body.MediaSid,
-  AccountSid = request.body.AccountSid,
-  AuthToken = request.body.AuthToken
+  MediaSid = request.body.MediaSid
 }
-response.message = Twilio.getMedia(parameters)
+return Twilio.getMedia(parameters)
 
 --#ENDPOINT POST /twilio/deleteMedia
 local parameters ={
   MessageSid = request.body.MessageSid,
-  MediaSid = request.body.MediaSid,
-  AccountSid = request.body.AccountSid,
-  AuthToken = request.body.AuthToken
+  MediaSid = request.body.MediaSid
 }
-response.message = Twilio.deleteMedia(parameters)
+return Twilio.deleteMedia(parameters)
 
 --#ENDPOINT GET /twilio/createTwiml
 xml = [[
@@ -312,24 +444,28 @@ xml = [[
     <Say voice="woman">You got a call!</Say>
 </Response>
 ]]
-response.message = xml
+return xml
 
 --#ENDPOINT GET /keystore/info
 response.message = Keystore.info()
 
---#ENDPOINT GET /email
-local parameters = {
-  from = "testing@exosite.com",
+--#ENDPOINT POST /email/send
+local toEmail = request.body.toEmail
+local subject = request.body.subject
+local text = request.body.text
+local html = request.body.html
+
+local emailData = {
   to = {
-    "testing@exosite.com",
+    toEmail,
   },
-  subject = "Hello",
-  text = "World",
+  subject = subject,
+  text = text,
+  html = html,
 }
+return Email.send(emailData)
 
-return  Email.send(parameters)
-
---#ENDPOINT GET /email/info
+--#ENDPOINT POST /email/info
 response.message = Email.getSettings()
 
 --#ENDPOINT POST /device/rpcCall
